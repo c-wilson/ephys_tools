54790d6a23aac7b89e295b6ca8c76a59c0d407b4
Chris Wilson <cdw291@nyu.edu>
2015-01-12 10:55:53 -0500

    Handle Arduino serial artifact better Arduino can start low before the first byte is transmitted, masking the first start bit. This now handles this condition by finding the end of the first word and estimating where the masked start bit should be.

--- data_processing/post_sort/serial_parser.py
+++ data_processing/post_sort/serial_parser.py
@@ -13,9 +13,14 @@ def parse_serial_stream(serial_stream, fs=20833., word_len=2, baudrate=300):
     :return:
     """
 
+    # NOTE: The bit spacing here is only approximately right due to a problem with arduino timing that makes the endbit
+    # between bytes longer than they should be (20% for 300 baud). Ideally we should parse bytes individually, but this
+    # works (at least with this baudrate). You would have to be 50% off before this should cause a problem because it is
+    # reading the bits from the center of where they should be.
+
     if serial_stream.ndim == 2:
         try:
-            shortdim = serial_stream.shape.find(1)
+            shortdim = serial_stream.shape.index(1)
             if shortdim == 1:
                 serial_stream = serial_stream[:, 0]
             else:
@@ -47,19 +52,37 @@ def parse_serial_stream(serial_stream, fs=20833., word_len=2, baudrate=300):
     start_id = downs-ups > (bit_len * word_len * (8 + start_bit + end_bit))
     start_samples = downs[start_id]
 
+
+    # Arduino software serial can start initially low before the first transmission, so the start detector above will fail.
+    # This means that the initial start bit is masked, as the signal is already low. We're going to try to find the
+    # end bit of this ridiculous first word, and work backward to find where this masked start bit occured.
+    if not log_stream[0]:
+        try:
+            for i in xrange(len(ups)-1):
+                if (ups[i+1] - ups[i]) > (bit_len * word_len * (8 + start_bit + end_bit)):
+
+                    firstword_end = ups[i]
+                    break
+        except IndexError:
+            raise ValueError('Serial parsing error: stream starts low, but cannot find end of first word.')
+        bts = word_len * (8+start_bit+end_bit) - 1 # end bit of the last byte IS the up, so we shouldn't count it.
+        firstword_start = firstword_end - (bts*bit_len)
+        firstword_start = np.int(firstword_start)
+    start_samples = np.concatenate(([firstword_start], start_samples))
+
     word_bits = bit_len * word_len * (8+start_bit+end_bit)
     bit_sample_spacing = np.linspace(bit_len/2, word_bits - bit_len/2, word_len * 10)
     bit_sample_spacing = bit_sample_spacing.round().astype(int)
 
-    _bits_tmp = np.zeros(bit_sample_spacing.shape, dtype=np.bool)
     _bytes = np.empty((word_len, 8), dtype=bool)
 
     trial_time_dict = {}
     for start in start_samples[:-1]:
-    #     print start
         bit_samples = bit_sample_spacing + start
         bits = log_stream[bit_samples]
         for i in range(word_len):
+            # This strips serial protocol bits assuming that a serial byte looks like: [startbit, 0, 1, ..., 7, endbit]
+            # as per normal serial com protocol. In other words: returned _bytes are 8 bit payloads.
             _bytes[i,:] = bits[i*9+1:(i+1)*9]
         number = _bytes_to_int(_bytes)
         trial_time_dict[number] = start
